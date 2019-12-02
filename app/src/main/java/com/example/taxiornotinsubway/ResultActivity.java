aa3613352636310def826cbb5047e8aea83f5b3c
Minwoo Kim <rlaalsdn456456@naver.com>
2019-12-02 09:23:05 +0900

    view tmap

--- app/src/main/java/com/example/taxiornotinsubway/ResultActivity.java
+++ app/src/main/java/com/example/taxiornotinsubway/ResultActivity.java
@@ -1,9 +1,12 @@
 package com.example.taxiornotinsubway;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.skt.Tmap.TMapView;
 
 public class ResultActivity extends AppCompatActivity {
     @Override 
@@ -11,7 +14,21 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_taxi);
         //map viewer
+        RelativeLayout relativeLayout = new RelativeLayout(this);
 
+        TMapView tmapview = new TMapView(this);
+
+        tmapview.setSKTMapApiKey("0f31e295-9ada-43b5-9292-5133678f2a00");
+        tmapview.setLanguage(TMapView.LANGUAGE_KOREAN);
+        tmapview.setIconVisibility(true);
+        tmapview.setZoomLevel(10);
+        tmapview.setMapType(TMapView.MAPTYPE_STANDARD);
+        tmapview.setCompassMode(true);
+        tmapview.setTrackingMode(true);
+
+        relativeLayout.addView(tmapview);
+
+        setContentView(relativeLayout);
         
         Bundle bundle = getIntent().getExtras();
         if(bundle != null){
