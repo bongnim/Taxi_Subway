e7438fbecf7fd4856275b37f7644594c102c13e3
jaehchangSCSC <jaeh_chang@outlook.kr>
2019-12-03 09:27:44 +0900

    1. Odsay Package, build.gradle 수정 2. Taxi 앱 연동 버튼 작성 3. PathSeeking activity 작성中...... -재혁-

--- app/src/main/java/com/example/taxiornotinsubway/MainActivity.java
+++ app/src/main/java/com/example/taxiornotinsubway/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.taxiornotinsubway;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -17,14 +21,44 @@ import android.view.View;
 
 import com.example.taxiornotinsubway.ui.main.SectionsPagerAdapter;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TabLayout tabLayout;
+
+    private static final String TaxiPackage = "com.skplanet.tmaptaxi.android.passenger";
+
     private int[] tabIcons = {
             R.drawable.ic_action_search,
             R.drawable.ic_action_history,
             R.drawable.ic_action_star,
     };
+    //택시앱이 존재하는지 확인하는 함수
+    public boolean getPackageList(String AppPackage) {
+        boolean isExist = false;
+
+        PackageManager pkgMgr = getPackageManager();
+        List<ResolveInfo> mApps;
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        mApps = pkgMgr.queryIntentActivities(mainIntent, 0);
+
+        try {
+            for (int i = 0; i < mApps.size(); i++) {
+                if(mApps.get(i).activityInfo.packageName.startsWith(AppPackage)){
+                    isExist = true;
+                    break;
+                }
+            }
+        }
+        catch (Exception e) {
+            isExist = false;
+        }
+        return isExist;
+    }//end of getPackageList
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,6 +94,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                if(getPackageList(TaxiPackage)){
+                    Intent intent = getPackageManager().getLaunchIntentForPackage(TaxiPackage);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }else{
+                    String url = "market://details?id=" + TaxiPackage;
+                    Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(i);
+                }
             }
         });
     }
